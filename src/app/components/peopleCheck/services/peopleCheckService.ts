f76bd17e411f62a3ef5ac754d79a4bcb3da963f3
Denys Soloviy <denys.soloviy@itera.no>
2018-02-05 15:48:07 +0200

    Refactored dates converting

--- src/app/components/peopleCheck/services/peopleCheckService.ts
+++ src/app/components/peopleCheck/services/peopleCheckService.ts
@@ -21,12 +21,9 @@ export class PeopleCheckService {
                 this.getADUser(tempEtWeb.LoginId).then(data => {
                     tempAD = data;
 
-                    tempEtWeb.HireDate = this.convertDate(tempEtWeb.HireDate);
-                    tempEtWeb.BirthDate = this.convertDate(tempEtWeb.BirthDate);
-                    tempAD.HireDate = this.convertDate(tempAD.HireDate);
-                    tempAD.BirthDate = this.convertDate(tempAD.BirthDate);
-                    tempMaconomy.HireDate = this.convertDate(tempMaconomy.HireDate);
-                    tempMaconomy.BirthDate = this.convertDate(tempMaconomy.BirthDate);
+                    this.setupDates(tempEtWeb);
+                    this.setupDates(tempAD);
+                    this.setupDates(tempMaconomy);
                     resolve(
                         {
                             etWebUser: tempEtWeb,
@@ -43,40 +40,6 @@ export class PeopleCheckService {
         return this._service.get("EtWeb", "GetEmployeeInfoByMaconomyId", {
             maconomyId: maconomyId
         });
-        // return new Promise<FullEmployee>(resolve => {
-        //     let promises: Promise<any>[] = [];
-        //     let tempFullEmployee: FullEmployee;
-        //     let tempEmployee: Employee;
-        //     let companies: ItemModel[];
-        //     let departments: ItemModel[];
-
-        //     promises.push(this._service.get("EtWeb", "GetEmployeeInfoByMaconomyId", {
-        //         maconomyId: maconomyId
-        //     }).then(data => tempFullEmployee = data));
-
-        //     promises.push(this._service.get("EtWeb", "GetEmployeeByMaconomyID", {
-        //         maconomyId: maconomyId
-        //     }).then(data => tempEmployee = data));
-
-        //     promises.push(this._service.get("EtWeb", "GetCompanies", {}).then(data => companies = data));
-
-        //     promises.push(this._service.get("EtWeb", "GetDepartments", {
-        //         companyId: tempEmployee.EtWebCompanyId
-        //     }).then(data => departments = data));
-
-        //     Promise.all(promises).then(() => {
-        //         tempFullEmployee.EtWebCompanyId = tempEmployee.EtWebCompanyId;
-        //         tempFullEmployee.EtWebDepartmentId = tempEmployee.EtWebDepartmentId;
-        //         tempFullEmployee.EtWebTeamId = tempEmployee.EtWebTeamId;
-        //         tempFullEmployee.JobTitle = tempEmployee.JobTitle;
-        //         tempFullEmployee.SupervisorMaconomyID = tempEmployee.SupervisorMaconomyID;
-
-        //         tempFullEmployee.CompanyName = companies.filter(x => x.Id === tempEmployee.EtWebCompanyId)[0].Name;
-        //         tempFullEmployee.DepartmentName = departments.filter(x => x.Id === tempEmployee.EtWebDepartmentId)[0].Name;
-
-        //         resolve(tempFullEmployee);
-        //     });
-        // });
     }
 
     getADUser(loginId: string): Promise<FullEmployee> {
@@ -91,13 +54,18 @@ export class PeopleCheckService {
         });
     }
 
-    convertDate(date: string) {
+    setupDates(obj: { HireDate: string, BirthDate: string }) {
+        obj.HireDate = this.convertDate(obj.HireDate);
+        obj.BirthDate = this.convertDate(obj.BirthDate);
+    }
+
+    convertDate(date: string): string {
         let ddate = new Date(date);
         let yyyy = ddate.getFullYear().toString();
         let mm = (ddate.getMonth() + 1).toString();
         let dd = ddate.getDate().toString();
 
-        return this.increaseStringTo(yyyy, 4) + "-" + this.increaseStringTo(mm, 2) + "-" + this.increaseStringTo(dd, 2);
+        return yyyy + "-" + this.increaseStringTo(mm, 2) + "-" + this.increaseStringTo(dd, 2);
     }
 
     increaseStringTo(str: string, length: number): string {
